8453ebec66932fbe5f521a70c3bbfa273badd260
Volodymyr Batsyk <vovabatsyk.lviv@gmail.com>
2021-06-03 14:43:34 +0300

    Add project files.

---
+++ MultMatrix/Matrix.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultMatrix
+{
+    public class Matrix
+    {
+        private static Matrix _instance;
+
+        private Matrix() { }
+
+        public static Matrix GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Matrix();
+            return _instance;
+        }
+
+
+        public int[,] GetMatrixFromConsole(string name)
+        {
+            Console.Write($"Кiлькiсть рядкiв матрицi: {name} ");
+            var n = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.Write($"Кiлькiсть стовпцiв матрицi: {name} ");
+            var m = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            var rnd = new Random();
+            var matrix = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = rnd.Next(1, 10);
+                }
+            }
+
+            return matrix;
+        }
+
+        public int[,] ParallelMatrixMultiplication(int[,] matrixA, int[,] matrixB)
+        {
+            int tLength = matrixA.GetLength(0);
+
+            if (matrixA.ColumnsCount() != matrixB.RowsCount())
+                throw new Exception("Множення не можливе!");
+
+            var matrixC = new int[matrixA.RowsCount(), matrixB.ColumnsCount()];
+
+            Parallel.For(0, tLength, delegate (int i)
+            {
+                for (int j = 0; j < tLength; j++)
+                {
+                    int tmp = 0;
+
+                    for (int k = 0; k < tLength; k++) tmp += matrixA[i, k] * matrixB[k, j];
+
+                    matrixC[i, j] = tmp;
+                }
+            });
+
+            return matrixC;
+        }
+
+        public int[,] ParallelMatrixMultiplication2(int[,] matrixA, int[,] matrixB)
+        {
+            int tLength = matrixA.GetLength(0);
+
+            if (matrixA.ColumnsCount() != matrixB.RowsCount())
+                throw new Exception("Множення не можливе!");
+
+            var matrixC = new int[matrixA.RowsCount(), matrixB.ColumnsCount()];
+
+            Parallel.For(0, tLength, i =>
+                Parallel.For(0, tLength, j =>
+            {
+                int tmp = 0;
+
+                Parallel.For(0, tLength, k => tmp += matrixA[i, k] * matrixB[k, j]);
+
+                matrixC[i, j] = tmp;
+            }));
+
+            return matrixC;
+        }
+        public int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+        {
+            if (matrixA.ColumnsCount() != matrixB.RowsCount())
+                throw new Exception("Множення не можливе!");
+
+            var matrixC = new int[matrixA.RowsCount(), matrixB.ColumnsCount()];
+
+            for (var i = 0; i < matrixA.RowsCount(); i++)
+            {
+                for (var j = 0; j < matrixB.ColumnsCount(); j++)
+                {
+                    matrixC[i, j] = 0;
+
+                    for (var k = 0; k < matrixA.ColumnsCount(); k++) matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                }
+            }
+
+            return matrixC;
+        }
+
+        public void PrintMatrix(int[,] matrix)
+        {
+            for (var i = 0; i < matrix.RowsCount(); i++)
+            {
+                for (var j = 0; j < matrix.ColumnsCount(); j++)
+                {
+                    Console.Write(matrix[i, j].ToString().PadLeft(4));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+    static class MatrixExt
+    {
+        public static int RowsCount(this int[,] matrix) => matrix.GetUpperBound(0) + 1;
+
+        public static int ColumnsCount(this int[,] matrix) => matrix.GetUpperBound(1) + 1;
+    }
+}
---
+++ MultMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MultMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            var matrixA = Matrix.GetInstance().GetMatrixFromConsole("A");
+            var matrixB = Matrix.GetInstance().GetMatrixFromConsole("B");
+
+            //Console.WriteLine("Матриця A:");
+            //Matrix.getInstance().PrintMatrix(matrixA);
+            //Console.WriteLine("Матриця B:");
+            //Matrix.getInstance().PrintMatrix(matrixB);
+
+            sw.Start();
+            var matrixC1 = Matrix.GetInstance().ParallelMatrixMultiplication(matrixA, matrixB);
+            sw.Stop();
+            Console.WriteLine($"Parallel: {sw.Elapsed}");
+
+            sw.Restart();
+            var matrixC2 = Matrix.GetInstance().ParallelMatrixMultiplication2(matrixA, matrixB);
+            sw.Stop();
+            Console.WriteLine($"Parallel2: {sw.Elapsed}");
+
+            sw.Restart();
+            var matrixC3 = Matrix.GetInstance().MatrixMultiplication(matrixA, matrixB);
+            sw.Stop();
+            Console.WriteLine($"Standart: {sw.Elapsed}");
+
+            //Console.WriteLine("Матриця C:");
+            //Matrix.getInstance().PrintMatrix(matrixC1);
+
+            Console.ReadKey();
+        }
+    }
+}
